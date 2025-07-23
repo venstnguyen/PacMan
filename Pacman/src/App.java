6790afb18aea9a3f390e928c3bf3b85e8a0f1948
venstnguyen@gmail.com <139813710+venstnguyen@users.noreply.github.com>
2025-07-23 17:27:55 -0600

    Restarting PacMan, leaving PNGs SN

--- Pacman/src/App.java
+++ Pacman/src/App.java
@@ -4,27 +4,5 @@ import javax.swing.JFrame; //Makes the window for the game
 
 public class App {
 	
-	public static void main(String[] args) throws Exception {
-		//window size
-		int rowCount = 21;
-		int columnCount = 19;
-		int tileSize = 32;
-		int boardWidth = columnCount * rowCount;
-		int boardHeight = rowCount * tileSize;
-		 
-		
-		//creating the window object
-		JFrame window = new JFrame("Pac-Man");
-		//window.setVisible(true);
-		window.setSize(boardWidth, boardHeight);
-		window.setLocationRelativeTo(null);
-		window.setResizable(true);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		PacMan pacmanGame = new PacMan();
-		window.add(pacmanGame);
-		window.pack();
-		window.setVisible(true);
-	}
 
 }
