ae543c78d48ab6ff1499a73e5e3449ffb0c62155
venstnguyen@gmail.com <139813710+venstnguyen@users.noreply.github.com>
2025-08-02 23:50:56 -0600

    Creating JFrame! SN

--- Pacman/src/App.java
+++ Pacman/src/App.java
@@ -3,6 +3,15 @@ import javax.swing.JFrame; //Makes the window for the game
 // Main file to run the game
 
 public class App {
+	
+	public static void main(String[] args) {
 	int rowCount = 21;
-
+	int columnCount = 19;
+	int tileSize = 32;
+	int boardWidth = columnCount * tileSize;
+	int boardHeight = rowCount * tileSize;
+	
+	JFrame frame = new JFrame("Pac Man");
+	frame.setVisible(true);
+	}
 }
